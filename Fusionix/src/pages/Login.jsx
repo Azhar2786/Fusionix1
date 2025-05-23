1a80455a850b14c457ff0493b3cd01d41838c44b
Azhar <darkcoders0101@gmail.com>
2025-05-23 17:49:17 +0530

    Update Login.jsx

--- Fusionix/src/pages/Login.jsx
+++ Fusionix/src/pages/Login.jsx
@@ -73,7 +73,8 @@ const Login = () => {
 
                     <p className="text-bodyLarge text-light-onSurfaceVariant
                     dark:text-dark-onSurfaceVariant mt-1 mb-5 text-center px-2">
-                        Enter your Fusionix account details.
+                        Enter your Fusionix account details.<br />
+                        For Testing, Email: whitedevils265@gmail.com, password: White@1234
                     </p>
 
                     <Form
